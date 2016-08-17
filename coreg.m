53c82186656e4737711ed6164066e83d7cb2f0ec
Fjaviervera <javierveraolmos@gmail.com>
2016-08-17 11:19:44 +0200

    changed to normal cc

--- coreg.m
+++ coreg.m
@@ -5,7 +5,7 @@ spm_jobman('initcfg');
 
 matlabbatch{1}.spm.spatial.coreg.estimate.ref = {ref};
 matlabbatch{1}.spm.spatial.coreg.estimate.source = {source};
-matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi';
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'ncc';
 matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep = [4 2];
 matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
 matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.fwhm = [7 7];
